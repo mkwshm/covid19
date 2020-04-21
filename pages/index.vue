47ef03521d3a5646e41b0cf1d2d56945a31fbde6
mkwshm <kawashima@godai.co.jp>
2020-04-21 22:11:32 +0900

    Add Card

--- pages/index.vue
+++ pages/index.vue
@@ -63,8 +63,8 @@ import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsC
 // import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
-// import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
-import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
+import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
+/// import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
 // import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
 // import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
 // import MetroCard from '@/components/cards/MetroCard.vue'
@@ -84,7 +84,8 @@ export default Vue.extend({
     ConfirmedCasesAttributesCard,
     AgeCard,
     HokenCard
-    // TestedNumberCard,InspectionPersonsNumberCard,
+    TestedNumberCard,
+    // InspectionPersonsNumberCard
     // TelephoneAdvisoryReportsNumberCard,
     // ConsultationDeskReportsNumberCard,
     // MetroCard,
